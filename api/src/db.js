fa1a44691468ebad44b99661f050ce9fe057ba5e
Flor Ciccione <flociccione@gmail.com>
2020-06-29 14:57:05 -0300

    muestra colores

--- api/src/db.js
+++ api/src/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 function db() {
-  return new Sequelize('postgres://postgres:123@localhost:5432/development', {
+  return new Sequelize('postgres://postgres:FUwezuda99&@localhost:5432/eCommerce', {
     logging: false, // set to console.log to see the raw SQL queries
    // native: true, // lets Sequelize know we can use pg-native for ~30% more speed
   });
