9482331ae89675349adfb09668bfcfe3f6e51dc0
leaarevalo <leaarevalo@outlook.com>
2020-07-04 11:32:13 -0300

    stash

--- client/src/App.js
+++ client/src/App.js
@@ -1,109 +1,128 @@
-import React, {useState, useEffect} from 'react';
-import { Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Route, Link } from "react-router-dom";
 //import { useSelector } from "react-redux";
 import { connect } from "react-redux";
-import { fetchProducts } from "./components/redux/actions/productsAction";
+import { fetchProducts } from "./components/Redux/actions/productsAction.js";
 
-import axios from 'axios';
+import axios from "axios";
 
 // Componentes
-import Product from './components/Product/Product.js';
-import Catalogo from './components/Catalogo/Catalogo.js';
-import CrudProduct from './components/CrudProduct/CrudProduct.js';
-import ProductCard from './components/Catalogo/ProductCard.js';
-import FormCreateUsuario from './components/CrudUsuario/FormCreateUsuario.js';
-import Carrito from './components/Carrito/Carrito.js';
+import Product from "./components/Product/Product.js";
+import Catalogo from "./components/Catalogo/Catalogo.js";
+import CrudProduct from "./components/CrudProduct/CrudProduct.js";
+import ProductCard from "./components/Catalogo/ProductCard.js";
+import FormCreateUsuario from "./components/CrudUsuario/FormCreateUsuario.js";
+import Carrito from "./components/Carrito/Carrito.js";
 
-function App({arrayProductos, fetchProducts}) {
+function App({ arrayProductos, fetchProducts }) {
+  var [categories, setCategories] = useState([]); //CUANDO SE HAGA EL GET DE CATEGORIAS ESTO YA NO VA MAS!!!!!!!!!!!!!
 
- var [categories, setCategories] = useState([]); //CUANDO SE HAGA EL GET DE CATEGORIAS ESTO YA NO VA MAS!!!!!!!!!!!!!
-  
   useEffect(() => {
     fetchProducts();
   }, []);
 
-
-//muestra todos los productos
-function showProducts(arrayProductos){
-  return arrayProductos.map(product => 
-  <Link to={'/producto/' + product.id} className="catalogo_product"> 
-      <ProductCard product={product}/>
-  </Link> );
-}; 
-//devuelve el producto buscado o mensaje 
-function onSearch(keyword) {
-  if(keyword){
-    var arraySearched = arrayProductos.filter(product => product.name.toLowerCase().includes(keyword.toLowerCase()) || product.keywords.toLowerCase().includes(keyword.toLowerCase()));
-    showProducts(arraySearched);
-  } else {
-    alert("No se han encontrado productos");
+  //muestra todos los productos
+  function showProducts(arrayProductos) {
+    return arrayProductos.map((product) => (
+      <Link to={"/producto/" + product.id} className="catalogo_product">
+        <ProductCard product={product} />
+      </Link>
+    ));
   }
-};
-//MUESTRA EN EL SELECT EL LISTADO DE LAS CATEGORIAS EXISTENTES
-function showCategoryOption(categories) { 
-  return categories.map(category => 
-  <option value={category.id} className='product_category_option'>
-      {category.name}
-  </option>
-  );
-};
-//devuelve los productos de la categoria seleccionada
-function onFilter(category) {
-  if(category){ 
-    var arrayFiltered = arrayProductos.filter(product => product.idCategory === parseInt(category)) 
-    if(arrayFiltered) {
-      showProducts(arrayFiltered);
+  //devuelve el producto buscado o mensaje
+  function onSearch(keyword) {
+    if (keyword) {
+      var arraySearched = arrayProductos.filter(
+        (product) =>
+          product.name.toLowerCase().includes(keyword.toLowerCase()) ||
+          product.keywords.toLowerCase().includes(keyword.toLowerCase())
+      );
+      showProducts(arraySearched);
+    } else {
+      alert("No se han encontrado productos");
+    }
+  }
+  //MUESTRA EN EL SELECT EL LISTADO DE LAS CATEGORIAS EXISTENTES
+  function showCategoryOption(categories) {
+    return categories.map((category) => (
+      <option value={category.id} className="product_category_option">
+        {category.name}
+      </option>
+    ));
+  }
+  //devuelve los productos de la categoria seleccionada
+  function onFilter(category) {
+    if (category) {
+      var arrayFiltered = arrayProductos.filter(
+        (product) => product.idCategory === parseInt(category)
+      );
+      if (arrayFiltered) {
+        showProducts(arrayFiltered);
+      } else {
+        alert("No se encontraron productos para esa categoría");
+      }
     } else {
       alert("No se encontraron productos para esa categoría");
     }
-  } else {
-    alert("No se encontraron productos para esa categoría");
-  }  
-}
-//devuelve el producto seleccionado
-function onSelect(id){
-  let producto = arrayProductos.filter(producto => producto.id === parseInt(id));
-  return producto[0];
-}; 
+  }
+  //devuelve el producto seleccionado
+  function onSelect(id) {
+    let producto = arrayProductos.filter(
+      (producto) => producto.id === parseInt(id)
+    );
+    return producto[0];
+  }
 
   return (
     <div className="App">
-       {/* PRODUCT Routes */}
-     <Route
-      exact
-      path='/'
-      component={() => <Catalogo arrayProductos={arrayProductos} showProducts={showProducts} onSearch={onSearch} onFilter={onFilter} categories={categories} showCategoryOption={showCategoryOption}/>}
-     />
-     <Route
-      exact
-      path='/producto/:id'
-      component={({match}) => <Product productDetail={onSelect(match.params.id)} />}
-     />
+      {/* PRODUCT Routes */}
+      <Route
+        exact
+        path="/"
+        component={() => (
+          <Catalogo
+            arrayProductos={arrayProductos}
+            showProducts={showProducts}
+            onSearch={onSearch}
+            onFilter={onFilter}
+            categories={categories}
+            showCategoryOption={showCategoryOption}
+          />
+        )}
+      />
+      <Route
+        exact
+        path="/producto/:id"
+        component={({ match }) => (
+          <Product productDetail={onSelect(match.params.id)} />
+        )}
+      />
 
-     {/* CRUD Routes */}
-     <Route
-      exact
-      path='/panel-admin/producto/'
-      component={() => <CrudProduct arrayProductos={arrayProductos} categories={categories} showCategoryOption={showCategoryOption}/>}
-     />
-     {/* CRUD USUARIO */}
+      {/* CRUD Routes */}
       <Route
-      exact
-      path='/usuario/registrarse/'
-      component={() => <FormCreateUsuario/>}
-     />
+        exact
+        path="/panel-admin/producto/"
+        component={() => (
+          <CrudProduct
+            arrayProductos={arrayProductos}
+            categories={categories}
+            showCategoryOption={showCategoryOption}
+          />
+        )}
+      />
+      {/* CRUD USUARIO */}
       <Route
-      exact
-      path='/usuario/cart/'
-      component={() => <Carrito/>}
-     />
-     
+        exact
+        path="/usuario/registrarse/"
+        component={() => <FormCreateUsuario />}
+      />
+      <Route exact path="/usuario/cart/" component={() => <Carrito />} />
     </div>
   );
 }
 const mapStateToProps = (state) => {
   return {
-      arrayProductos: state.products.products,
+    arrayProductos: state.products.products,
   };
 };
 
--- client/src/components/Catalogo/Catalogo.js
+++ client/src/components/Catalogo/Catalogo.js
@@ -1,40 +1,50 @@
 import React from "react";
 
-
 // CSS
 import "./Catalogo.css";
 
 // Components
-import CategoryFilter from '../CategoryFilter/CategoryFilter.js';
-import SearchBar from '../SearchBar/SearchBar.js';
-import NavBar from '../NavBar/NavBar.js';
+import CategoryFilter from "../CategoryFilter/CategoryFilter.js";
+import SearchBar from "../SearchBar/SearchBar.js";
+import NavBar from "../NavBar/NavBar.js";
+import { useSelector } from "react-redux";
+export default function Catalogo({
+  arrayProductos,
+  showProducts,
+  onFilter,
+  categories,
+  showCategoryOption,
+  onSearch,
+}) {
+  const products = useSelector((state) => products.data);
+  return (
+    <div className="catalogo">
+      <NavBar arrayProductos={arrayProductos} showProducts={showProducts} />
+      <div className="catalogo_bg"></div>
 
-export default function Catalogo({arrayProductos, showProducts, onFilter, categories, showCategoryOption, onSearch}){
+      <div className="catalogo_title">
+        <h1>Shop Online</h1>
+        <h5>
+          En nuestra tienda vas a encontrar accesorios de diseño exclusivo,
+          confeccionados a mano con una visión que conjuga moda, belleza y
+          pasión.
+        </h5>
+      </div>
 
-    return(
-        
-        <div className="catalogo">
-            <NavBar arrayProductos={arrayProductos} showProducts={showProducts}/>
-            <div className="catalogo_bg"></div>
+      <div className="catalogo_bar">
+        <div className="volver_catalogo_bar">Volver al listado completo</div>
+        <CategoryFilter
+          onFilter={onFilter}
+          showProducts={showProducts}
+          categories={categories}
+          showCategoryOption={showCategoryOption}
+        />
+        <SearchBar showProducts={showProducts} onSearch={onSearch} />
+      </div>
 
-            <div className="catalogo_title">
-                <h1>Shop Online</h1>
-                <h5>En nuestra tienda vas a encontrar accesorios de diseño exclusivo, confeccionados a mano con una visión que conjuga moda, belleza y pasión.</h5>
-            </div>
-
-            <div className="catalogo_bar">
-                <div className="volver_catalogo_bar">Volver al listado completo</div>
-                <CategoryFilter onFilter={onFilter} showProducts={showProducts} categories={categories} showCategoryOption={showCategoryOption}/>
-                <SearchBar showProducts={showProducts} onSearch={onSearch}/>
-            </div>
-            
-            <div className="container">
-                <div className="catalogo_products">
-                    {showProducts(arrayProductos)}
-                </div>
-            </div>
-
+      <div className="container">
+        <div className="catalogo_products">{showProducts(products)}</div>
+      </div>
     </div>
   );
 }
-
